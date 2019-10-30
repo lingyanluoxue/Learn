2c180d4ad520f34c1842dfab78c4445112268dd1
zhanghongmei <zhanghongmei@taoche.com>
2019-10-30 15:10:03 +0800

    组合

--- src/main/java/com/lylx/learn/Combination.java
+++ src/main/java/com/lylx/learn/Combination.java
@@ -4,37 +4,44 @@ package com.lylx.learn;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Test {
+public class Combination {
     public static void main(String[] args) {
 
         int str1= Integer.parseInt("101", 2);
         System.out.println(str1+"");
 
 
-        List<String> result = new ArrayList<>();
+//        List<String> result = new ArrayList<>();
+//
+//        String str[] = { "A", "B", "C" };
+//
+//        int nCnt = str.length;
+//
+//        int nBit = (0xFFFFFFFF >>> (32 - nCnt));
+//
+//        for (int i = 1; i <= nBit; i++) {
+//            boolean flag  = false;
+//            StringBuilder temp = new StringBuilder();
+//            for (int j = 0; j < nCnt; j++) {
+//                if ((i << (31 - j)) >> 31 == -1) {
+//                    if(flag){
+//                        temp.append(",");
+//                    }
+//                    temp.append(str[j]);
+//                    flag = true;
+//                }
+//            }
+//            if (temp.toString().contains("A")){
+//                result.add(temp.toString());
+//                System.out.println(temp);
+//            }
+//        }
 
         String str[] = { "A", "B", "C" };
-
-        int nCnt = str.length;
-
-        int nBit = (0xFFFFFFFF >>> (32 - nCnt));
-
-        for (int i = 1; i <= nBit; i++) {
-            boolean flag  = false;
-            StringBuilder temp = new StringBuilder();
-            for (int j = 0; j < nCnt; j++) {
-                if ((i << (31 - j)) >> 31 == -1) {
-                    if(flag){
-                        temp.append(",");
-                    }
-                    temp.append(str[j]);
-                    flag = true;
-                }
-            }
-            if (temp.toString().contains("A")){
-                result.add(temp.toString());
-                System.out.println(temp);
-            }
+//        List<String> list = getCombinationList(str,"A");
+        List<String> list = getCombinationList(str,"");
+        for (String s : list) {
+            System.out.println(s);
         }
 
     }
@@ -42,7 +49,7 @@ public class Test {
     /**
      * 获取组合列表
      */
-    private List<String> getCombinationList(String[] element, String containKeyWord) {
+    private static List<String> getCombinationList(String[] element, String containKeyWord) {
         List<String> result = new ArrayList<>();
         int nCnt = element.length;
         int nBit = (0xFFFFFFFF >>> (32 - nCnt));
